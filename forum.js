e25a610c247acc102781b542e752672aa595c10c
Mason <bevinsm355@gmail.com>
2025-03-11 22:02:22 -0400

    created html and js file and connedcted both to each other

---
+++ forum.js
@@ -0,0 +1,11 @@
+const freelancers = [
+    {name: "Alice", occupation: "Writer", starting: 30},
+    {name: "Bob", occupation: "Teacher", starting: 50}
+    {name: "Carol", occupation: "Programmer", starting: 70}
+]
+
+function init() {
+    //Step 1: grab div with the id of "root"
+
+    //Step 2: 
+}
